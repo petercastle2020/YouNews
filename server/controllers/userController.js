42bd4136010b5fadfaa88c509b6ff8a457ee9712
Peter <vagnerpetel2020@gmail.com>
2023-02-12 17:17:49 -0300

    add, signup and login with jwt.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,23 @@
-const userModel = require("../models/userModel");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({_id: _id}, process.env.JWT_SECRET, {expiresIn: "90d"} )
+}
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ msg: "login user" });
+  const {email, password} = req.body;
+
+  try {
+    const user = await User.login(email, password)
+
+    // create jwt token.
+    const token = createToken(user._id)
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 };
 // signup user
 const signupUser = async (req, res) => {
@@ -11,7 +25,11 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    // create jwt token.
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
