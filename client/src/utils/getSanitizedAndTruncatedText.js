b714d8ec292b10f46a44024c07896d07cd1fb000
Peter <vagnerpetel2020@gmail.com>
2023-03-15 18:21:00 -0300

    change in the getSanitizedAndTruncatedText function.

--- client/src/utils/getSanitizedAndTruncatedText.js
+++ client/src/utils/getSanitizedAndTruncatedText.js
@@ -3,7 +3,10 @@ import DOMPurify from "dompurify";
 export const getSanitizedAndTruncatedText = (content, maxLength = Infinity) => {
   const sanitizedContent = DOMPurify.sanitize(content);
   const truncatedSanitizedContent = sanitizedContent.substr(0, maxLength);
-  if (typeof maxLength === "number" && maxLength > 0) {
+
+  if (maxLength === Infinity) {
+    return truncatedSanitizedContent;
+  } else if (typeof maxLength === "number" && maxLength > 0) {
     return truncatedSanitizedContent + "...";
   } else {
     return truncatedSanitizedContent;
