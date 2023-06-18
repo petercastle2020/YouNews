e64bec95ddd2eaf34ad892fe6194069b59130409
Peter <vagnerpetel2020@gmail.com>
2023-06-18 09:58:35 -0300

    clean up.

--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ const app = express();
 app.use(express.json());
 
 // Serve static files from the build directory
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Fall back to index.html for any other route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 /*
-for reference when dev
+for testing requests.
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
